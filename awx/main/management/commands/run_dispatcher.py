1a284064a4fe91c0d87f6351ffab07f03c46f95e
Ryan Petrello <rpetrell@redhat.com>
2018-08-13 17:13:49 -0400

    use celerybeat for scheduling (for now)

--- awx/main/management/commands/run_dispatcher.py
+++ awx/main/management/commands/run_dispatcher.py
@@ -1,11 +1,12 @@
 # Copyright (c) 2015 Ansible, Inc.
 # All Rights Reserved.
 import logging
-import time
-import threading
+from multiprocessing import Process
 
 from django.conf import settings
+from django.core.cache import cache as django_cache
 from django.core.management.base import BaseCommand
+from django.db import connection as django_connection
 from kombu import Connection, Exchange, Queue
 
 from awx.main.dispatch import get_local_queuename, reaper
@@ -22,28 +23,44 @@ class Command(BaseCommand):
     help = 'Launch the task dispatcher'
 
     def beat(self):
-        '''
-        <Ralph Wiggum voice> I'm a periodic scheduler!
-        Seriously, though, this needs to be replaced with something to give us
-        what celerybeat used to provide.  For _now_, this is good enough to
-        make the TaskManager run every X seconds.
+        from celery import app
+        from celery.beat import PersistentScheduler
+        from celery.apps import beat
 
-        A good candidate might be: https://apscheduler.readthedocs.io/en/latest/
-        '''
-        start = time.time()
-        while True:
-            time.sleep(1)
-            elapsed = int(time.time() - start)
-            for _, v in settings.CELERYBEAT_SCHEDULE.items():
-                if elapsed % v['schedule'].total_seconds() == 0:
-                    task = TaskWorker.resolve_callable(v['task'])
-                    task.apply_async()
+        class AWXScheduler(PersistentScheduler):
+            def setup_schedule(self):
+                super(AWXScheduler, self).setup_schedule()
+                self.update_from_dict(settings.CELERYBEAT_SCHEDULE)
+
+            def apply_async(self, entry, publisher, **kwargs):
+                task = TaskWorker.resolve_callable(entry.task)
+                result, queue = task.apply_async()
+
+                class TaskResult(object):
+                    id = result['uuid']
+
+                return TaskResult()
+
+        app = app.App()
+        app.conf.BROKER_URL = settings.BROKER_URL
+        app.conf.CELERY_TASK_RESULT_EXPIRES = False
+        beat.Beat(
+            30,
+            app,
+            schedule='/var/lib/awx/beat.db', scheduler_cls=AWXScheduler
+        ).run()
 
     def handle(self, *arg, **options):
-        reaper.reap()
-        beat = threading.Thread(target=self.beat)
+        # It's important to close these because we're _about_ to fork, and we
+        # don't want the forked processes to inherit the open sockets
+        # for the DB and memcached connections (that way lies race conditions)
+        django_connection.close()
+        django_cache.close()
+        beat = Process(target=self.beat)
         beat.daemon = True
         beat.start()
+
+        reaper.reap()
         consumer = None
         with Connection(settings.BROKER_URL) as conn:
             try:
